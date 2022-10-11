8c13533d71e595ef832abae188c287a270323f42
Greg Stickel <gregory.stickel@live.com>
2022-10-11 10:11:14 -0400

    Updated 10/11/2022 10:11am

--- phoneNumber.js
+++ phoneNumber.js
@@ -1,11 +1,10 @@
+function convertPhone(array) {
+  array.splice(0, 0, "(");
+  array.splice(4, 0, ")");
+  array.splice(5, 0, " ");
+  array.splice(9, 0, "-");
+  let result = array.join("");
+  return result;
+}
 
-function convertPhone (array){
-array.splice(0,0,"(");
-array.splice(4,0,")");
-array.splice(5,0," ");
-array.splice(9,0,"-");
-let result = array.join(''); 
-return result;
-};
-
-console.log(convertPhone([1,2,3,4,5,6,7,8,9,0]))
+console.log(convertPhone([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
